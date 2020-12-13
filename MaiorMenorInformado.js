d3fa0c5e2e8cb3964d081891ded96dcb18d2f921
Tati Oliveira <tat1roliv@users.noreply.github.com>
2020-12-13 19:30:42 -0300

    add 1
    maior menor

---
+++ MaiorMenorInformado.js
@@ -0,0 +1,27 @@
+//solicita 10 numeros e informa qual o maior e o menor valor digitado
+
+//var
+var cont = 1
+var numG
+var numP
+//inicio
+var num = parseInt(prompt('digite o ' + cont + "  numero : "))
+numG = num
+numP = num
+cont = cont + 1
+//teste maior menor
+while (cont<=10){
+  var num = parseInt(prompt('digite o ' + cont + "  numero : "))
+  
+  if (num>=numG){
+    numG = num
+     }
+  else if (num<=numP){
+      numP = num
+    }
+    cont = cont + 1
+  }
+//resultado
+alert('o maior número digitado foi: ' + numG)
+alert('o menor número digitado foi: ' + numP)
+//fim
